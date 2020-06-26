42e8477a531234e73b4dfca41523bea3888ea0c8
vedank1997-mishra <56275577+vedank1997-mishra@users.noreply.github.com>
2020-06-26 23:30:18 +0530

    Injection

--- src/main/java/technicalblog/TechnicalBlogApplication.java
+++ src/main/java/technicalblog/TechnicalBlogApplication.java
@@ -2,10 +2,12 @@ package technicalblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class TechnicalBlogApplication {
+@ComponentScan
+public class TanechnicalBlogApplication {
     public static void main(String [] args){
-        SpringApplication.run(TechnicalBlogApplication.class, args);
+        SpringApplication.run(TanechnicalBlogApplication.class, args);
     }
 }
--- src/main/java/technicalblog/controller/HomeController.java
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -1,39 +1,49 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import technicalblog.model.Post;
+import technicalblog.service.PostService;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 @Controller
 public class HomeController {
 
+    public HomeController() {
+        System.out.println("*** HomeController ***");
+    }
+
+    @Autowired
+    private PostService postService;
+
     @RequestMapping("/")
     public String getAllPosts(Model model) {
 
-        ArrayList<Post> posts = new ArrayList<>();
+        ArrayList<Post> posts = postService.getAllPosts();
 
-        Post post1 = new Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post Body 1");
-        post1.setDate(new Date());
-
-        Post post2 = new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post Body 2");
-        post2.setDate(new Date());
-
-        Post post3 = new Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post Body 3");
-        post3.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
+//        ArrayList<Post> posts = new ArrayList<>();
+//
+//        Post post1 = new Post();
+//        post1.setTitle("Post 1");
+//        post1.setBody("Post Body 1");
+//        post1.setDate(new Date());
+//
+//        Post post2 = new Post();
+//        post2.setTitle("Post 2");
+//        post2.setBody("Post Body 2");
+//        post2.setDate(new Date());
+//
+//        Post post3 = new Post();
+//        post3.setTitle("Post 3");
+//        post3.setBody("Post Body 3");
+//        post3.setDate(new Date());
+//
+//        posts.add(post1);
+//        posts.add(post2);
+//        posts.add(post3);
 
 
         model.addAttribute("posts",posts);
